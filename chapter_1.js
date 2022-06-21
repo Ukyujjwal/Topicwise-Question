7802b24921ac87af82360937f098ef16faa7a9af
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-06-21 20:16:32 +0530

    Module_1

---
+++ chapter_1.js
@@ -0,0 +1,69 @@
+//  Declaration and initialization
+
+// var a = "Jack ";
+// console.log(a);
+// Declaration
+// var a;
+
+// initialization
+// a = "Jack";
+
+//  issue with of var
+/*var a = "Jack";
+    var someBool = true;
+    if(someBool) {
+        var a = "ujjwal"
+    }
+    console.log(a);*/
+
+// use of let keyword
+ /*let firsta = "Jack";
+    let someBool = true;
+    if(someBool) {
+       let firsta = "ujjwal"
+        // console.log(firsta);
+    }
+    console.log(firsta);*/
+
+//Use of const keyword
+/*const a = "Jack";
+    let someBool = true;
+    if(someBool){
+        const a = "ujjwal"
+    }
+    console.log(a);
+    console.log(a);*/
+    
+//  a is already been declare
+  /*const a = "Jack";
+  const a = "ujjwal;
+  console.log(a);*/
+ 
+//   js data type
+ /*let names = ["Swati", "neha", "Jack"];
+ console.log(names);
+
+ var ages = [1, 2, 3];
+ console.log(ages);*/
+
+//  Objects
+   /* let Employee = {
+        name : "Jack",
+        age : 25,
+        id : 23569454,
+        department : "Mechanical",
+        city : "Jamalpur"
+     }
+    console.log(Employee);*/
+
+// Function
+/*function sum (a, b) {
+    return  a+b
+}
+console.log(sum(2, 4));*/
+
+// Conversion
+/*let result;
+  result = 1 + null;
+  console.log(result);*/
+    
