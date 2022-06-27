87b09f5a3b72f7c5dfbafbd7ad33c939dc9699ca
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-06-27 17:53:22 +0530

    Module_2

---
+++ String_operator.js
@@ -0,0 +1,16 @@
+//  Creating a string
+/*const string = 'The revolution will not be televised.';
+ console.log(string);
+
+// if yoy previously defined the variable string
+ const badString = string;
+ console.log(badString);*/
+
+// Escape
+  /*const bigmouth = 'I\'ve got no right to take my place...'
+    console.log(bigmouth);*/
+
+// Use of back tick ` and $()
+ /*const name = 'Jack';
+ const greeting = `Hello, ${name}`;
+ console.log(greeting);*/ 
