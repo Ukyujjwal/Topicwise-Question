7eef040eecfae1c5f4afd68ba10db98fa67a4f55
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-06-29 18:28:12 +0530

    module_2

--- String_operator.js
+++ String_operator.js
@@ -14,3 +14,97 @@
  /*const name = 'Jack';
  const greeting = `Hello, ${name}`;
  console.log(greeting);*/ 
+
+  /*const one = 'Where are you!';
+  const two = 'I,am at jamalpur';
+  const join = `${one}${two}`;
+  console.log(join);*/
+
+  /*const button = document.querySelector('button');
+  function greet() {
+  const name = prompt('What is your name?');
+  alert(`Hello ${name}, nice to see you!`);
+}  
+    button.addEventListener('click', greet);*/
+
+
+    /*const firstname = 'Jack';
+    const lastname = 254;
+    console.log(firstname+' '+lastname);*/
+
+//Simply we use Number() to covert anything into number & use toString() to convert anything into string 
+/*const myString = '123';
+const myNum = toString(myString);
+console.log(typeof myNum);*/
+
+// including expression
+/*const song = 'Love Dose';
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of
+ ${score/highestScore * 100}%. `;
+ console.log(output);*/
+
+//  Multiline String
+ /*const output = `Samsung is a good phone.\nI love samsung a lot!`;
+ console.log(output);*/
+
+// find lenght,char of the string
+/*const String = 'Samsung';
+console.log(String.length); 
+console.log(String[6]);
+console.log(String[String.length-3]);*/
+
+  
+// find largest num among three
+/*function max_of_three(a, b, c)
+  {
+    max_val = 0;
+    if (a > b){
+      max_val = a;
+    } else
+    {
+      max_val = b;
+    }
+    if (c > max_val){
+      max_val = c;
+    }
+    return max_val;
+
+  }// program to display the sum of natural numbers
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0, i = 1;
+
+// looping from i = 1 to number
+while(i <= number) {
+    sum += i;
+    i++;
+}
+  console.log(max_of_three(0, 2, 9));*/
+
+   /*let num = 100;
+   let sum = 0;
+   for (let i = 1; i<= num; i++) {
+     sum += i;
+    
+   }
+   console.log(sum);*/
+  /*  function sum(n) {
+     return n*(n+1)/2;
+   }
+   let n = 7;
+   console.log(sum(n));*/
+
+//find subString use include() method
+   /*const browserType = 'mozilla';
+
+   if (browserType.includes('zilla')) {
+     console.log('Found zilla!');
+   } else {
+     console.log('No zilla here!');
+   }*/
+var b = 10;
+console.log(--b);
