1b99747035dd5189f6e058168349349ac5b2723e
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-07-19 14:29:15 +0530

    module_4

---
+++ Objects&JSON.js
@@ -0,0 +1,27 @@
+// Create Object
+/*const student = {
+    firstName:'Ram',
+    class:10
+};
+console.log(student);
+console.log(student.firstName);
+console.log(typeof student);*/
+
+/*const person = {
+    name: ["Bob", "Smith"],
+    age: 32,
+    bio: function() {
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);},
+    introduceSelf: function() {
+     console.log(`Hi! I'm ${this.name[0]}.`);}
+    };
+console.log(person.name);
+console.log(person.name[0]);
+console.log(person.age);
+person.bio();
+person.introduceSelf();*/
+// we can also use bio() instead of bio: function
+
+
+// One use of bracket notation is that it can be used to set not only member values dynamically, but member names too.
+
